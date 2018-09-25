accc530cbb9502db422d075fa71baae6122c8e12
wp <501098462@qq.com>
2018-09-25 15:13:59 +0800

    优化

--- src/base/16正则表达式.py
+++ src/base/16正则表达式.py
@@ -138,9 +138,9 @@ def main():
     """
     finditer  和 findall 类似，在字符串中找到正则表达式所匹配的所有子串，并把它们作为一个迭代器返回。
     """
-    it = re.finditer(r"\d+","12a32bc43jf3")
+    it = re.finditer(r"\d+", "12a32bc43jf3")
     for match in it:
-        print (match.group() )
+        print(match.group())
 
     pass
 
