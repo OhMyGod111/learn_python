c5b3c6c0978f81a602894f83eb2fd6695120ca6f
wp <501098462@qq.com>
2018-09-26 23:46:31 +0800

    优化..

---
+++ src/utils/line.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+def line(describe = ''):
+    print("\n")
+    print("---------------------" + describe + "------------------------------>>>")
+
+
+# 斐波那契(fibonacci)数列模块
+def fib(n):  # 定义到 n 的斐波那契数列
+    a, b = 0, 1
+    while b < n:
+        print(b, end=' ')
+        a, b = b, a + b
+    print()
+
+
+def fib2(n):  # 返回到 n 的斐波那契数列
+    result = []
+    a, b = 0, 1
+    while b < n:
+        result.append(b)
+        a, b = b, a + b
+    return result
+
+
+if __name__ == '__main__':
+    # main()
+    # print("程序自身在运行")
+    pass
+else:
+    # print("我来自另一模块")
+    pass
